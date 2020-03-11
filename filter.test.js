3fcdce24ce2f50e5adbcdf523bd7b7205d98f052
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-11 12:59:38 +0200

    add testcase 31:compare columnValue by 'Starts with' filter1Value Or 'Starts with' filter2Value

--- filter.test.js
+++ filter.test.js
@@ -475,4 +475,20 @@ describe("filter Rows according to a column value  ", () => {
     ];
     expect(result).toStrictEqual(expected);
   });
+  test("testcase 31:compare columnValue by 'Starts with' filter1Value Or 'Starts with' filter2Value", () => {
+    let filterValues = {
+      filter1By: "Starts with",
+      filter1Value: "A",
+      filter2By: "Starts with",
+      filter2Value: "B",
+      compareValue: "Or",
+      column: "name"
+    };
+    const result = filterRows(mycolumn, filterValues);
+    const expected = [
+      { id: "2", name: "Ahmad khdair", category: "male" },
+      { id: "3", name: "Ban Afanah", category: "FEMALE" }
+    ];
+    expect(result).toStrictEqual(expected);
+  });
 });
