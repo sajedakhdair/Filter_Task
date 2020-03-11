d4699875085c0122427e67347bf594c1d1056b11
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-11 12:45:53 +0200

    add testcase 29:compare columnValue by 'Starts with' filter1Value Or 'Is not equal to' filter2Value

--- filter.test.js
+++ filter.test.js
@@ -442,4 +442,21 @@ describe("filter Rows according to a column value  ", () => {
     const expected = [{ id: "4", name: "SAJED Abd", category: "Male" }];
     expect(result).toStrictEqual(expected);
   });
+  test("testcase 29:compare columnValue by 'Starts with' filter1Value Or 'Is not equal to' filter2Value", () => {
+    let filterValues = {
+      filter1By: "Starts with",
+      filter1Value: "S",
+      filter2By: "Is not equal to",
+      filter2Value: "Ban Afanah",
+      compareValue: "Or",
+      column: "name"
+    };
+    const result = filterRows(mycolumn, filterValues);
+    const expected = [
+      { id: "1", name: "sajeda KHDAIR", category: "female" },
+      { id: "2", name: "Ahmad khdair", category: "male" },
+      { id: "4", name: "SAJED Abd", category: "Male" }
+    ];
+    expect(result).toStrictEqual(expected);
+  });
 });
