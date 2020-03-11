ccafb1cae23e9bd17ca3b04bdcbb757d330b621c
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-11 15:06:45 +0200

    add testcase 46:compare columnValue by 'Contains' filter1Value And 'Does not contain' filter2Value

--- filter.test.js
+++ filter.test.js
@@ -702,4 +702,17 @@ describe("filter Rows according to a column value  ", () => {
     ];
     expect(result).toStrictEqual(expected);
   });
+  test("testcase 46:compare columnValue by 'Contains' filter1Value And 'Does not contain' filter2Value", () => {
+    let filterValues = {
+      filter1By: "Contains",
+      filter1Value: "Khdair",
+      filter2By: "Does not contain",
+      filter2Value: "Sajeda",
+      compareValue: "And",
+      column: "name"
+    };
+    const result = filterRows(mycolumn, filterValues);
+    const expected = [{ id: "2", name: "Ahmad khdair", category: "male" }];
+    expect(result).toStrictEqual(expected);
+  });
 });
