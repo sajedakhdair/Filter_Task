057c0ac8bafa6b4a5a7c0a520d6ce789e89f8e42
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-15 10:50:33 +0200

    add modification#8 (create function 'prepareFinalResult')

--- filter.ts
+++ filter.ts
@@ -97,15 +97,19 @@ const removeDuplicateElements = (myArray) => {
   return finalResult;
 }
 
+const prepareFinalResult=(firstFilterResult: Column[],secondFilterResult: Column[])=>
+{
+  let finalResult1 = mergeTwoArray(firstFilterResult, secondFilterResult);
+  let sortedArray = SortArrayById(finalResult1);
+  let finalResult = removeDuplicateElements(sortedArray);
+  return finalResult;
+}
 const filterWithOr = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
   const column: keyof Column = filterValues.column;
   const filter2By = filterValues.filter2By;
   const filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
   let secondFilterResult = myfilter(rowsToFilter, filter2By, filter2Value_LowerCase, column);
-  let finalResult1 = mergeTwoArray(firstFilterResult, secondFilterResult);
-  let sortedArray = SortArrayById(finalResult1);
-  let finalResult = removeDuplicateElements(sortedArray);
-  return finalResult;
+  return prepareFinalResult(firstFilterResult, secondFilterResult);
 }
 
 const secondFilter = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
