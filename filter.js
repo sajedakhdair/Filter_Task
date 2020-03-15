77c292b71c7b5ed0e26a37134dd889d829c155a6
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-15 11:41:10 +0200

    the compiled code in JS after add modification#10

--- filter.js
+++ filter.js
@@ -5,33 +5,21 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
             r[k] = a[j];
     return r;
 };
-var filterIsEqualTo = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) {
-        return row[column].toLowerCase() === filterValue;
-    });
+var filterRows = function (rows, filterValues) {
+    var rowsToFilter = rows;
+    var filter1Value = filterValues.filter1Value;
+    if (!filter1Value) {
+        return rowsToFilter;
+    }
+    var firstFilterResult = firstFilter(rowsToFilter, filterValues);
+    var finalResult = secondFilterController(rowsToFilter, firstFilterResult, filterValues);
+    return finalResult;
 };
-var filterIsNotEqualTo = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) {
-        return row[column].toLowerCase() !== filterValue;
-    });
-};
-var filterStartsWith = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) {
-        return row[column].toLowerCase().startsWith(filterValue);
-    });
-};
-var filterContains = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) {
-        return row[column].toLowerCase().includes(filterValue);
-    });
-};
-var filterDoesNotContains = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) {
-        return !row[column].toLowerCase().includes(filterValue);
-    });
-};
-var filterEndsWith = function (rowsToFilter, filterValue, column) {
-    return rowsToFilter.filter(function (row) { return row[column].toLowerCase().endsWith(filterValue); });
+var firstFilter = function (rowsToFilter, filterValues) {
+    var column = filterValues.column;
+    var filter1By = filterValues.filter1By;
+    var filter1Value_LowerCase = filterValues.filter1Value.toLowerCase();
+    return filterOptionsController(rowsToFilter, filter1By, filter1Value_LowerCase, column);
 };
 var filterOptionsController = function (rowsToFilter, filterBy, filterValue, column) {
     var filterOperations = {
@@ -44,44 +32,25 @@ var filterOptionsController = function (rowsToFilter, filterBy, filterValue, col
     };
     return filterOperations[filterBy];
 };
-var firstFilter = function (rowsToFilter, filterValues) {
-    var column = filterValues.column;
-    var filter1By = filterValues.filter1By;
-    var filter1Value_LowerCase = filterValues.filter1Value.toLowerCase();
-    return filterOptionsController(rowsToFilter, filter1By, filter1Value_LowerCase, column);
+var filterIsEqualTo = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) { return row[column].toLowerCase() === filterValue; });
 };
-var filterWithAndFinalResult = function (firstFilterResult, filterValues) {
-    var column = filterValues.column;
-    var filter2By = filterValues.filter2By;
-    var filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
-    if (firstFilterResult === [])
-        return [];
-    return filterOptionsController(firstFilterResult, filter2By, filter2Value_LowerCase, column);
+var filterIsNotEqualTo = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) { return row[column].toLowerCase() !== filterValue; });
 };
-var mergeTwoArray = function (firstArray, secondArray) {
-    return __spreadArrays(firstArray, secondArray);
+var filterStartsWith = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) { return row[column].toLowerCase().startsWith(filterValue); });
 };
-var sortArrayById = function (myArray) {
-    return myArray.sort(function (FirstItem, SecondItem) {
-        return Number(FirstItem.id) - Number(SecondItem.id);
+var filterContains = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) { return row[column].toLowerCase().includes(filterValue); });
+};
+var filterDoesNotContains = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) {
+        return !row[column].toLowerCase().includes(filterValue);
     });
 };
-var removeDuplicateElements = function (myArray) {
-    var finalResult = myArray.filter(function (elem, index, self) { return self.findIndex(function (item) { return (item.id === elem.id); }) === index; });
-    return finalResult;
-};
-var prepareFinalResult = function (firstFilterResult, secondFilterResult) {
-    var mergedResult = mergeTwoArray(firstFilterResult, secondFilterResult);
-    var sortedResult = sortArrayById(mergedResult);
-    var finalResult = removeDuplicateElements(sortedResult);
-    return finalResult;
-};
-var filterWithOrController = function (rowsToFilter, firstFilterResult, filterValues) {
-    var column = filterValues.column;
-    var filter2By = filterValues.filter2By;
-    var filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
-    var secondFilterResult = filterOptionsController(rowsToFilter, filter2By, filter2Value_LowerCase, column);
-    return prepareFinalResult(firstFilterResult, secondFilterResult);
+var filterEndsWith = function (rowsToFilter, filterValue, column) {
+    return rowsToFilter.filter(function (row) { return row[column].toLowerCase().endsWith(filterValue); });
 };
 var secondFilterController = function (rowsToFilter, firstFilterResult, filterValues) {
     var filter2Value = filterValues.filter2Value;
@@ -93,14 +62,37 @@ var secondFilterController = function (rowsToFilter, firstFilterResult, filterVa
     }
     return filterWithOrController(rowsToFilter, firstFilterResult, filterValues);
 };
-var filterRows = function (rows, filterValues) {
-    var rowsToFilter = rows;
-    var filter1Value = filterValues.filter1Value;
-    if (!filter1Value) {
-        return rowsToFilter;
-    }
-    var firstFilterResult = firstFilter(rowsToFilter, filterValues);
-    var finalResult = secondFilterController(rowsToFilter, firstFilterResult, filterValues);
+var filterWithAndFinalResult = function (firstFilterResult, filterValues) {
+    var column = filterValues.column;
+    var filter2By = filterValues.filter2By;
+    var filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
+    if (firstFilterResult === [])
+        return [];
+    return filterOptionsController(firstFilterResult, filter2By, filter2Value_LowerCase, column);
+};
+var filterWithOrController = function (rowsToFilter, firstFilterResult, filterValues) {
+    var column = filterValues.column;
+    var filter2By = filterValues.filter2By;
+    var filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
+    var secondFilterResult = filterOptionsController(rowsToFilter, filter2By, filter2Value_LowerCase, column);
+    return prepareFinalResult(firstFilterResult, secondFilterResult);
+};
+var prepareFinalResult = function (firstFilterResult, secondFilterResult) {
+    var mergedResult = mergeTwoArray(firstFilterResult, secondFilterResult);
+    var sortedResult = sortArrayById(mergedResult);
+    var finalResult = removeDuplicateElements(sortedResult);
+    return finalResult;
+};
+var mergeTwoArray = function (firstArray, secondArray) {
+    return __spreadArrays(firstArray, secondArray);
+};
+var sortArrayById = function (myArray) {
+    return myArray.sort(function (FirstItem, SecondItem) {
+        return Number(FirstItem.id) - Number(SecondItem.id);
+    });
+};
+var removeDuplicateElements = function (myArray) {
+    var finalResult = myArray.filter(function (elem, index, self) { return self.findIndex(function (item) { return (item.id === elem.id); }) === index; });
     return finalResult;
 };
 module.exports = {
