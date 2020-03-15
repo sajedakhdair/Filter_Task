c214702b29ea9ade8b4a8472267bc1b3f5a6d566
sajedakhdair <sajeda.khdair@gmail.com>
2020-03-15 11:14:21 +0200

    add modification#9 (rename some functions and some constans)

--- filter.ts
+++ filter.ts
@@ -53,7 +53,7 @@ const filterEndsWith = (rowsToFilter, filterValue, column) => {
     row => row[column].toLowerCase().endsWith(filterValue))
 }
 
-const myfilter = (rowsToFilter, filterBy, filterValue, column) => {
+const filterOptionsController = (rowsToFilter, filterBy, filterValue, column) => {
   const filterOperations = {
     "Is equal to": filterIsEqualTo(rowsToFilter, filterValue, column),
     "Is not equal to": filterIsNotEqualTo(rowsToFilter, filterValue, column),
@@ -69,23 +69,23 @@ const firstFilter = (rowsToFilter: Column[], filterValues: FilterFormValues) => 
   const column: keyof Column = filterValues.column;
   const filter1By = filterValues.filter1By;
   const filter1Value_LowerCase = filterValues.filter1Value.toLowerCase();
-  return myfilter(rowsToFilter, filter1By, filter1Value_LowerCase, column);
+  return filterOptionsController(rowsToFilter, filter1By, filter1Value_LowerCase, column);
 }
 
-const filterWithAnd = (firstFilterResult: Column[], filterValues: FilterFormValues) => {
+const filterWithAndFinalResult = (firstFilterResult: Column[], filterValues: FilterFormValues) => {
   const column: keyof Column = filterValues.column;
   const filter2By = filterValues.filter2By;
   const filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
   if (firstFilterResult === [])
     return [];
-  return myfilter(firstFilterResult, filter2By, filter2Value_LowerCase, column);
+  return filterOptionsController(firstFilterResult, filter2By, filter2Value_LowerCase, column);
 }
 
 const mergeTwoArray = (firstArray, secondArray) => {
   return [...firstArray, ...secondArray];
 }
 
-const SortArrayById = (myArray) => {
+const sortArrayById = (myArray) => {
   return myArray.sort((FirstItem, SecondItem) => {
     return Number(FirstItem.id) - Number(SecondItem.id);
   });
@@ -97,30 +97,30 @@ const removeDuplicateElements = (myArray) => {
   return finalResult;
 }
 
-const prepareFinalResult=(firstFilterResult: Column[],secondFilterResult: Column[])=>
-{
-  let finalResult1 = mergeTwoArray(firstFilterResult, secondFilterResult);
-  let sortedArray = SortArrayById(finalResult1);
-  let finalResult = removeDuplicateElements(sortedArray);
+const prepareFinalResult = (firstFilterResult: Column[], secondFilterResult: Column[]) => {
+  let mergedResult = mergeTwoArray(firstFilterResult, secondFilterResult);
+  let sortedResult = sortArrayById(mergedResult);
+  let finalResult = removeDuplicateElements(sortedResult);
   return finalResult;
 }
-const filterWithOr = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
+
+const filterWithOrController = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
   const column: keyof Column = filterValues.column;
   const filter2By = filterValues.filter2By;
   const filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
-  let secondFilterResult = myfilter(rowsToFilter, filter2By, filter2Value_LowerCase, column);
+  let secondFilterResult = filterOptionsController(rowsToFilter, filter2By, filter2Value_LowerCase, column);
   return prepareFinalResult(firstFilterResult, secondFilterResult);
 }
 
-const secondFilter = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
-  const filter2Value_LowerCase = filterValues.filter2Value.toLowerCase();
+const secondFilterController = (rowsToFilter: Column[], firstFilterResult: Column[], filterValues: FilterFormValues) => {
+  const filter2Value = filterValues.filter2Value;
   const compareValue = filterValues.compareValue;
-  if (!filter2Value_LowerCase)
+  if (!filter2Value)
     return firstFilterResult;
   if (compareValue === "And") {
-    return filterWithAnd(firstFilterResult, filterValues);
+    return filterWithAndFinalResult(firstFilterResult, filterValues);
   }
-  return filterWithOr(rowsToFilter, firstFilterResult, filterValues);
+  return filterWithOrController(rowsToFilter, firstFilterResult, filterValues);
 }
 
 const filterRows = (rows: Column[], filterValues: FilterFormValues): Column[] => {
@@ -130,9 +130,10 @@ const filterRows = (rows: Column[], filterValues: FilterFormValues): Column[] =>
     return rowsToFilter;
   }
   let firstFilterResult = firstFilter(rowsToFilter, filterValues);
-  let finalResult = secondFilter(rowsToFilter, firstFilterResult, filterValues);
+  let finalResult = secondFilterController(rowsToFilter, firstFilterResult, filterValues);
   return finalResult;
 };
+
 module.exports = {
   filterRows
 };
